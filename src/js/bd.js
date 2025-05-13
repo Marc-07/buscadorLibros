91bf58ff73c05bee70a690bfc8de567cec5f8c6a
Maria <maanraca01@gmail.com>
2025-05-13 11:11:26 -0500

    Se suben archivos del proyecto

---
+++ src/js/bd.js
@@ -0,0 +1,63 @@
+const libros = [
+    {
+      titulo: "Cien años de soledad",
+      autor: "Gabriel García Márquez",
+      genero: "Ficción",
+      precio: 12000
+    },
+    {
+      titulo: "1984",
+      autor: "George Orwell",
+      genero: "Ciencia Ficción",
+      precio: 10000
+    },
+    {
+      titulo: "Orgullo y prejuicio",
+      autor: "Jane Austen",
+      genero: "Romance",
+      precio: 15000
+    },
+    {
+      titulo: "El nombre del viento",
+      autor: "Patrick Rothfuss",
+      genero: "Fantasía",
+      precio: 20000
+    },
+    {
+      titulo: "Sapiens: De animales a dioses",
+      autor: "Yuval Noah Harari",
+      genero: "No Ficción",
+      precio: 12000
+    },
+    {
+      titulo: "El principito",
+      autor: "Antoine de Saint-Exupéry",
+      genero: "Ficción",
+      precio: 10000
+    },
+    {
+      titulo: "Crónica de una muerte anunciada",
+      autor: "Gabriel García Márquez",
+      genero: "Ficción",
+      precio: 12000
+    },
+    {
+      titulo: "Dune",
+      autor: "Frank Herbert",
+      genero: "Ciencia Ficción",
+      precio: 20000
+    },
+    {
+      titulo: "Harry Potter y la piedra filosofal",
+      autor: "J.K. Rowling",
+      genero: "Fantasía",
+      precio: 12000
+    },
+    {
+      titulo: "La sombra del viento",
+      autor: "Carlos Ruiz Zafón",
+      genero: "Ficción",
+      precio: 10000
+    }
+  ];
+  
